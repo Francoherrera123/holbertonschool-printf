14fb674ac7f306485553490b2cb79c553ab96fb0
root <Francoherrera123>
2023-04-02 19:23:10 -0700

    final version

--- main_controller.c
+++ main_controller.c
@@ -56,11 +56,10 @@ int main_controller(char *format, va_list args)
 {
 	int i = 0, count = 0;
 
-	if (format[i] == '\0')
-		return (0);
-
 	if (format == NULL)
 		return (-1);
+	if (format == '\0')
+		return (count);
 
 	while (format[i] != '\0')
 	{
